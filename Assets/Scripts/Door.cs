cbee1472fe73e339902add4841fcf029c2ec8190
Rodrigo Sanches Devigo <rsdevigo@gmail.com>
2023-03-13 20:55:38 -0400

    Update 2023

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Door :MonoBehaviour, IInteractive
 {
     public bool isOpen = false;
+    [SerializeField]
+    private Animator doorUpper;
+    [SerializeField]
+    private Animator doorLower;
     public void Interact()
     {
         isOpen = true;
+        doorUpper.SetBool("isOpen", isOpen);
+        doorLower.SetBool("isOpen", isOpen);
+        StartCoroutine("Open");
+    }
+
+    IEnumerator Open(){
+        yield return new WaitForSeconds(0.5f);
         gameObject.SetActive(false);
     }
 
